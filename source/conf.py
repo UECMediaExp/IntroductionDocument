1561fa31aa0408ebc68478bd4df4c37d642b2fad
Hayaru SHOUNO <shouno@uec.ac.jp>
2021-01-21 12:46:15 +0900

    ソースのみてくれ変更

--- source/conf.py
+++ source/conf.py
@@ -51,7 +51,8 @@ templates_path = ['_templates']
 # You can specify multiple suffix as a list of string:
 #
 # source_suffix = ['.rst', '.md']
-source_suffix = '.rst'
+# source_suffix = '.rst'
+source_suffix = ['.rst', '.md']
 
 # The master toctree document.
 master_doc = 'index'
@@ -69,8 +70,8 @@ language = 'ja'
 exclude_patterns = []
 
 # The name of the Pygments (syntax highlighting) style to use.
-pygments_style = 'sphinx'
-
+#pygments_style = 'sphinx'
+pygments_style = 'monokai'
 
 # -- Options for HTML output -------------------------------------------------
 
